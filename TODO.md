afc33658e26d4e02eaaf8839e6449e1ca6be3fa8
Zachary Morel <morelz133@gmail.com>
2018-06-21 10:40:57 -0400

    Added more steps to TODOs, and comments explaining Webpack config

--- TODO.md
+++ TODO.md
@@ -1,3 +1,12 @@
-Node v8 engine still doesn't properly support es6 module loading Import/ Export.  The underlying Common js api still has built a proper bridge.  
-So I turned off the Node Eslint plugin. 
+TODO: 
+- Enable cache busting in Webpack
+- CSS file bundling using css-loader in webpack
+- Enable source maps in webpack
+- Add debug commands to package.json
+- Hot module replacement
+- Auto-restart web server when changes are detected via nodemon
+
+
+NOTES: 
+Node v8 engine still doesn't properly support es6 module loading Import/ Export.  The underlying Common js api still hasn't built a proper bridge.  As a result, I turned off the Node Eslint plugin. 
  
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+const HtmlWebPackPlugin = require('html-webpack-plugin') 
+/*
+  nodeExternals: EXCLUDES node_modules for wepack build.
+  HtmlWebPackPlugin: The plugin will generate an HTML5 file for you that includes all your webpack bundles in the body using script tags 
+    & CSS files via link tags. 
+*/ 
 
 const moduleObj = {
   rules: [
     {
       test: /\.js$/,
       exclude: /node_modules/,
-      loaders: ["babel-loader"],
+      loaders: ["babel-loader"], // Loaders allow you to pre-process files as you import or “load” them. 
     }
   ]
 }
@@ -15,16 +20,16 @@ const moduleObj = {
 module.exports = [
   // CLIENT
   {
-    entry: {
+    entry: {  // The Entry point for Webpack to being it's dependency graph of our files.
       'client': './src/client/index.js',
     },
-    target: 'web',
-    mode: 'development', 
-    output: {
+    target: 'web', // Target specifies what environment webpack should compile for, eg, Node || Web
+    mode: 'development', // Providing the mode configuration option tells webpack to use its built-in optimizations accordingly.
+    output: { // output configuration options tells webpack how to write the compiled files to disk. NOTE: only 1 output per webpack modular bundler.
       filename: '[name].js',
       path: path.resolve(__dirname, 'dist/public')
     },
-    module: moduleObj,
+    module: moduleObj, // The object where we specify what rules and Loaders are needed for Webpack's bundling 
     plugins: [
       new HtmlWebPackPlugin({
         template: 'src/client/index.html'
@@ -43,6 +48,6 @@ module.exports = [
       path: path.resolve(__dirname, 'dist')
     },
     module: moduleObj,
-    externals: [nodeExternals()]
+    externals: [nodeExternals()] // externals configuration option provides a way of excluding dependencies from the output bundles.
   }
 ]
