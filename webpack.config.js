0c866f9f684a47aaba346274ab7bd847ac43cefa
Zachary Morel <morelz133@gmail.com>
2018-06-21 10:11:45 -0400

    Added Webpack static HTML plugin to feed static files and read them.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 const moduleObj = {
-  loaders: [
+  rules: [
     {
       test: /\.js$/,
       exclude: /node_modules/,
@@ -11,29 +12,37 @@ const moduleObj = {
   ]
 }
 
-const client = {
-  entry: {
-    'client': './src/client/index.js',
+module.exports = [
+  // CLIENT
+  {
+    entry: {
+      'client': './src/client/index.js',
+    },
+    target: 'web',
+    mode: 'development', 
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist/public')
+    },
+    module: moduleObj,
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: 'src/client/index.html'
+      })
+    ]
   },
-  target: 'web',
-  output: {
-    filename: '[name].js',
-    path: path.reslove(__dirname, 'dist/public')
-  },
-  module: moduleObj
-}
-
-const server = {
-  entry: {
-    'server': './src/server/index.js'
-  },
-  target: 'node',
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: moduleObj,
-  externals: [nodeExternals()]
-}
-
-module.exports = [client, server]
+  // SERVER
+  {
+    entry: {
+      'server': './src/server/index.js'
+    },
+    target: 'node',
+    mode: 'development', 
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    module: moduleObj,
+    externals: [nodeExternals()]
+  }
+]
